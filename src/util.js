0fa00d46e43e2719fdfaa45b6e0c58d9f01b1596
zasoms <623064100@qq.com>
2024-07-15 17:19:26 +0800

    test: 添加文件压缩调试

--- src/util.js
+++ src/util.js
@@ -5,6 +5,12 @@ module.exports = {
     // 把所有编译好的代码打包，方便上传
     var zip = new adm_zip();
     zip.addLocalFolder(sourcePath);
-    zip.writeZip(targetPath);
+    zip.writeZip(targetPath, function (error) {
+      if (error) {
+        console.log('发生错误:', error)
+      } else {
+        console.log('创建成功:')
+      }
+    });
   },
 };
